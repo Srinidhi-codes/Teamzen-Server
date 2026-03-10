daf16ce132523f126c9ab974ba04e52b0c63c502
Srinidhi-codes <66681637+Srinidhi-codes@users.noreply.github.com>
2026-03-10 21:52:18 +0530

    fix(email): Complete refactor of tasks.py for robust imports and reliability

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -2,6 +2,8 @@ from celery import shared_task
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils import timezone
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from datetime import timedelta
 import socket
 import re
